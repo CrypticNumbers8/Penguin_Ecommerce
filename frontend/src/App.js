e79d931da9f943785c1b4976718074d95dddc048
Aniket Karna <aniketkarna@gmail.com>
2023-12-18 02:15:49 -0700

    patanahi

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Penguin</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
